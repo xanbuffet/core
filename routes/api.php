dd4289a1d67fe998a9b4d2d0c60455c8acedf422
Brevis Nguyen <brevisnguyen@gmail.com>
2025-06-08 16:42:41 +0800

    update route resource

--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,27 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\MenuController;
 use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
-
 
 /* == Auth routes == */
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 /* == Menu routes == */
-Route::get('/menus', [MenuController::class, 'index']);
-Route::get('/menus/{day}', [MenuController::class, 'show']);
+Route::resource('menus', MenuController::class)->only(['index', 'show']);
 
-Route::middleware('auth:sanctum')->group(function() {
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::get('/orders/{id}', [OrderController::class, 'show']);
-    Route::post('/orders', [OrderController::class, 'store']);
+    Route::resource('users', UserController::class)->only([
+        'index', 'show', 'update', 'destroy',
+    ]);
+
+    Route::resource('orders', OrderController::class)->only([
+        'show', 'store', 'update', 'destroy',
+    ]);
 });
 
 Route::post('/webhook', WebhookController::class);
