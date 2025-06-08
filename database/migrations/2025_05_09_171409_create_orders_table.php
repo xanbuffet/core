99a4c9c2cc81ebb93b7d6b1c0d2a911d98243560
Brevis Nguyen <brevisnguyen@gmail.com>
2025-06-08 16:43:17 +0800

    add order number

--- database/migrations/2025_05_09_171409_create_orders_table.php
+++ database/migrations/2025_05_09_171409_create_orders_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('order_no', 20)->unique();
             $table->foreignId('user_id')->cascadeOnDelete();
             $table->decimal('total_price')->default(0.00);
             $table->text('notes')->nullable();
