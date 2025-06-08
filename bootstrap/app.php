9678e723c48847c212f38abb0f1ebf4e49c18142
Brevis Nguyen <brevisnguyen@gmail.com>
2025-06-08 16:43:51 +0800

    add middleware to api route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
-        $middleware->append(StartSession::class);
+        $middleware->api(append: StartSession::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
